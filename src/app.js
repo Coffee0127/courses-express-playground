ccf0e8e69ac556dfb61dc4f05de49b9e1587ad9f
Bo-Xuan Fan <coffee0127@gmail.com>
2022-06-19 21:12:14 +0800

    Add courses api

--- src/app.js
+++ src/app.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/courses', (req, res) => {
+  res.status(200)
+    .send({
+      '1': {
+        name: 'Calculus',
+        credit: 3,
+        description: 'The mathematical study of continuous change.'
+      },
+      '2': {
+        name: 'Object Oriented Programming',
+        credit: 4,
+        description: 'A programming paradigm based on the concept of "objects", which can contain data and code.'
+      }
+    })
 })
 
 app.listen(port, () => {
